ebfd96e8eabf4a3f1d66b7eb585806057d40b5a9
Ashley Michal <ashleymichal@gmail.com>
2015-12-01 19:42:30 -0600

    logout enabled and tested

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
 
-  # # Logs in the given user.
-  # def log_in(user)
-  #   session[:user_id] = user.id
-  # end
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
   # Returns the current logged-in user (if any).
   def current_user
@@ -12,6 +12,12 @@ module SessionsHelper
 
   def logged_in?
   	!current_user.nil?
+  end
+  
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 	
 end
