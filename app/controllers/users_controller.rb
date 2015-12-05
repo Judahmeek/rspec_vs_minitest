a1fb2ab23b21b05c194b0548cd9c1df4a335151e
Ashley Michal <ashleymichal@gmail.com>
2015-12-05 12:46:44 -0600

    Complete: Update User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 	
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+      @user = User.find(params[:id])
+  end
 
   def new
   	@user = User.new
@@ -13,15 +13,29 @@ class UsersController < ApplicationController
   	if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
+      redirect_to @user
   	else
-  		render 'new'
+      render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
 
-	  def user_params
-	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
