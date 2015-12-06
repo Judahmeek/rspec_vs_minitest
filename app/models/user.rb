f10e16c5fdae34d157426d756045b504e69926da
Ashley Michal <ashleymichal@gmail.com>
2015-12-06 13:14:29 -0600

    added microposts model; added microposts to user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
 										
 	has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :microposts, dependent: :destroy
 
   # Returns the hash digest of the given string.
   def User.digest(string)
