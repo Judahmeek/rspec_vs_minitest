77a42337f9b775ebe0ca1b61752ab3153f4d3ddb
Ashley Michal <ashleymichal@gmail.com>
2015-12-11 17:08:27 -0600

    still working out how to test title tag

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -16,6 +16,11 @@ describe StaticPagesController do
         get page
         expect(response).to render_template("static_pages/#{page}")
       end
+
+      # it "Renders the correct title" do
+      #   get page
+      #   expect(response).to have_title("#{page.capitalize} | Ruby on Rails Tutorial Sample App")
+      # end
     end
   end
 
