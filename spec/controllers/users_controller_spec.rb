9ae0bc6a7e100b0a5ba156c8198782509cd12c6a
Judah Meek <judah.meek@gmail.com>
2016-08-25 22:23:24 +0000

    refactored user_cntr_spec w/ fixtures

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,20 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  
-  around do |spec|
-    @spec_user = User.create( name: "example",
-                              email: "example@example.com",
-                              password_digest: User.digest('password'),
-                              admin: true,
-                              activated: true,
-                              activated_at: Time.zone.now)
-    spec.run
-    @spec_user.destroy
-  end
+  fixtures :users
   
   it "should let users log in" do
-    log_in_as(@spec_user)
-    expect(subject.current_user).to eq(@spec_user)
+    log_in_as(users(:michael))
+    expect(subject.current_user).to eq(users(:michael))
   end
 end
