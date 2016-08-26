a3c609a66cadbd726e55b6569b614e37bc4013ca
Judah Meek <judah.meek@gmail.com>
2016-08-26 22:11:54 +0000

    minor changes

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -20,9 +20,9 @@ class MicropostsControllerTest < ActionController::TestCase
 		assert_redirected_to login_url
 	end
 
-	test "should redirect destroy for wrong micropost" do
-		log_in_as(users(:michael))
-		micropost = microposts(:ants)
+	test "should redirect destroy for another user's micropost" do
+		log_in_as(users(:michael)) # This is confusing. Michael is an admin so shouldn't he be able to delete other user's posts?
+		micropost = microposts(:ants) # this micropost doesn't belong_to michael
 		assert_no_difference 'Micropost.count' do
 			delete :destroy, id: micropost
 		end
