3f2e0959bf8f1043ecf96cca4d8d8f2887285b5b
Judah Meek <judah.meek@gmail.com>
2016-08-27 17:28:43 +0000

    changed spaces to tabs for /test/*

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class RelationshipsControllerTest < ActionController::TestCase
 
-    test "create should require logged-in user" do
-        assert_no_difference 'Relationship.count' do
-            post :create  # params don't matter because create request should be caught by logged_in_user before_action
-        end               # except... you can't properly test for a change in the relationship count by an unauthenticated user without them
-        assert_redirected_to login_url
-    end
+		test "create should require logged-in user" do
+				assert_no_difference 'Relationship.count' do
+						post :create  # params don't matter because create request should be caught by logged_in_user before_action
+				end               # except... you can't properly test for a change in the relationship count by an unauthenticated user without them
+				assert_redirected_to login_url
+		end
 
-    test "destroy should require logged-in user" do
-        assert_no_difference 'Relationship.count' do
-            delete :destroy, id: relationships(:one)
-        end
-        assert_redirected_to login_url
-    end
+		test "destroy should require logged-in user" do
+				assert_no_difference 'Relationship.count' do
+						delete :destroy, id: relationships(:one)
+				end
+				assert_redirected_to login_url
+		end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,26 +5,26 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+	# Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+	fixtures :all
 
-  # Returns true if a test user is logged in.
-  def is_logged_in?
-    !session[:user_id].nil?
-  end
+	# Returns true if a test user is logged in.
+	def is_logged_in?
+		!session[:user_id].nil?
+	end
 
-  # Logs in as a test user.
-  def log_in_as(user, options = {})
-  	password		= options[:password]		|| 'password'
-  	remember_me = options[:remember_me]	|| '1'
-  	if integration_test?
-  		post login_path, session: { email:      user.email,
-  									              password:   password,
-  									              remember_me: remember_me }
+	# Logs in as a test user.
+	def log_in_as(user, options = {})
+		password		= options[:password]		|| 'password'
+		remember_me = options[:remember_me]	|| '1'
+		if integration_test?
+			post login_path, session: { email:      user.email,
+																	password:   password,
+																	remember_me: remember_me }
 		else
 			session[:user_id] = user.id
-    end
-  end
+		end
+	end
 
 	private
 
